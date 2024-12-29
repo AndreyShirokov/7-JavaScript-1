842eee6e757abec11c7c1165775dee1f839fd2f5
andre <mail4andre@yandex.ru>
2024-12-30 00:51:21 +0300

    Арифметические операторы

---
+++ app1.js
@@ -0,0 +1,28 @@
+// базовые арифметические операторы
+// это однострочный комментрарий
+
+/*
+это комментрарий 
+в две строки
+*/
+
+const width = 10;
+const heigth = 5;
+const space = width * heigth;
+console.log(space);
+
+const newwidth = width - 4;
+const newheigth = heigth + 4;
+const division = newwidth / newheigth;
+console.log(division);
+
+const volume = 2 * 2 * 2;
+console.log(volume);
+
+const volume2 = 2 ** 3;
+console.log(volume2);
+
+// строки
+const city = 'Москва';
+const street = 'Новослободская';
+console.log(city + ', ' + street + ', ' + 5);
