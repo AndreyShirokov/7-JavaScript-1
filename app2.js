d39e6979093a380f40c113e1591fa045a233ae47
andre <mail4andre@yandex.ru>
2024-12-31 02:23:52 +0300

    операторы присваивания и сравнения

---
+++ app2.js
@@ -0,0 +1,19 @@
+let age = 18;
+age += 2; // age = age + 2
+age -= 3; // age = age - 3
+
+age *= 3; // age = age * 2
+age /= 2; // age = age / 3
+
+age++ // age = age + 1
+age-- // age = age - 1
+
+console.log(age);
+
+// операторы сравнения
+const vasia = 20; // Васе 20 лет
+console.log(age > vasia); // Вася моложе
+console.log(age >= vasia); 
+console.log(age < vasia); 
+console.log(age <= vasia); 
+console.log(age == vasia); 
