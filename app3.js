62e0a6110c2e3453fc215ca48d3480f18404aae4
andre <mail4andre@yandex.ru>
2025-01-02 19:02:00 +0300

    порядок вычислений

---
+++ app3.js
@@ -0,0 +1,23 @@
+// порядок вычислений
+// isSuited - подходит (ящик влезает в коробку) 
+const box = 100;
+const size = 90;
+const isSuited = box - 10 > size - 5;
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Operators/Operator_Precedence#table
+// сначала выполняются операции с более высоким приоритетом 12 а поом 10 
+// 1. box - 10 (приоритет 12)
+// 2. size - 5 (приоритет 12)
+// 3. > - сравнение (приоритет 10)
+console.log(isSuited);
+
+const a = 6 + 10 / 2; //  10 / 2 = 5 next 6 + 5 = 11
+console.log(a);
+
+const x = (6 + 10) / 2; //  10 + 6 = 16 next 16 / 2 = 8
+console.log(x);
+
+let b;
+let c;
+c = b = 100 + 50;
+console.log(c);
+console.log(b);
